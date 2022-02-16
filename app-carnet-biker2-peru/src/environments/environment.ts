6a9275bf1def8fb44cb0edb31fa9b4f0f6873c71
kpalomino2020 <kenedy.sistemas@gmail.com>
2022-02-15 19:42:27 -0500

    Instalando firebase, firestore, build, deploy

--- app-carnet-biker2-peru/src/environments/environment.ts
+++ app-carnet-biker2-peru/src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'proyect-biker',
+    appId: '1:693570867545:web:c54668a62e1758d0a8bc48',
+    storageBucket: 'proyect-biker.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyD9ck37LDxz0XUlT-eZnScrFpTfdcGjOXo',
+    authDomain: 'proyect-biker.firebaseapp.com',
+    messagingSenderId: '693570867545',
+    measurementId: 'G-K41NPHQ2DH',
+  },
   production: false
 };
 
